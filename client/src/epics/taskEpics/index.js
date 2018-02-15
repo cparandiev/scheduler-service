e68c29c9b67a72692e7248e7cb9e481521ff4bf9
Roberto Aleksandrov <roberto.a@sbtech.com>
2018-02-15 15:32:34 +0200

    modified epics

--- client/src/epics/taskEpics/index.js
+++ client/src/epics/taskEpics/index.js
@@ -1,13 +1,12 @@
-import { Observable } from 'rxjs';
 import { get } from '../../data/fakeData';
 import { TASK_ACTIONS, TASK_URLS } from '../../constants/TasksConstants';
-import { getAllTasksFulfilled, getAllTasksRejected } from '../../actions/taskActions'
+import { getAllTasksFulfilled, getAllTasksRejected } from '../../actions/taskActions';
+import { process } from '../epicsHelper';
 
-export const getResourcesNames = action$ =>
-  action$.ofType(TASK_ACTIONS.GET_ALL)
-    .flatMap(action => 
-        get(`${TASK_URLS.GET_ALL}`)
-    )
-    .map(processedData =>{ console.log(processedData); return getAllTasksFulfilled(processedData)})
-    .catch(error =>
-        Observable.of(getAllTasksRejected(error)));
+export const getResourcesNames = action$ => {
+    const { GET_ALL } = TASK_ACTIONS;
+    const URL = TASK_URLS.GET_ALL
+    const action = action$.ofType(GET_ALL.ACTION).flatMap(action => get(URL));
+    
+    return process(action, GET_ALL);      
+}
